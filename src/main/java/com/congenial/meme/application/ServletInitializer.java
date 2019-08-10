1a0fadf6358a67d422762bfa6f553085331e4414
Mathias Barreneche <mathias.barreneche@gmail.com>
2019-08-10 15:38:09 -0300

    project initial skeleton

---
+++ src/main/java/com/congenial/meme/application/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.congenial.meme.application;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(Application.class);
+    }
+}
